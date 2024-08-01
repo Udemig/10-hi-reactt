9dcb109dba3b7b3669f4e4953bd4dd238ee40a85
furkanevin <furkanevin00@gmail.com>
2024-08-01 22:20:21 +0300

    soslar tamamlandı

--- 10_unit_test/2_ice_cream_app/src/components/Scoops/Scoops.test.jsx
+++ 10_unit_test/2_ice_cream_app/src/components/Scoops/Scoops.test.jsx
@@ -1,5 +1,6 @@
-import { getByText, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Scoops from "./index";
+import userEvent from "@testing-library/user-event";
 
 /*
  ! Seçiciler
@@ -27,6 +28,9 @@ test("API'dan alınan veriler için ekrana kartlar basılır", async () => {
 });
 
 test("Çeşitlerin ekleme ve azaltma işlevlerinin toplam fiyata etkisi", async () => {
+  // userEvent'in kurulumunu yap
+  const user = userEvent.setup();
+
   // test edilecek bileşen render edilir
   render(<Scoops />);
 
@@ -38,24 +42,35 @@ test("Çeşitlerin ekleme ve azaltma işlevlerinin toplam fiyata etkisi", async 
   const total = screen.getByTestId("total");
 
   // başlangıç anında toplam 0 mı kontrol et
+  expect(total).toHaveTextContent(/^0$/);
 
   // choclate'ın ekle butonuna tıkla
+  await user.click(addBtns[2]);
 
   // toplam fiyat 20 mi kontrol et
+  expect(total).toHaveTextContent(/^20$/);
 
   // vanilla'nın ekle butonuna çift tıkla
+  await user.dblClick(addBtns[1]);
 
   // toplam fiyat  60 mı kontrol et
+  expect(total).toHaveTextContent(/^60$/);
 
   // vanilla'nın azalt butonuna tıkla
+  await user.click(delBtns[1]);
 
   // toplam fiyat  40 mı kontrol et
+  expect(total).toHaveTextContent(/^40$/);
 
   // vanilla'nın azalt butonuna tıkla
+  await user.click(delBtns[1]);
 
   // toplam fiyat  20 mı kontrol et
+  expect(total).toHaveTextContent(/^20$/);
 
   // chocalate'ın azalt butonuna tıkla
+  await user.click(delBtns[2]);
 
   // toplam fiyat 0 mı kontrol et
+  expect(total).toHaveTextContent(/^0$/);
 });
