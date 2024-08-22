8d1703313eba8ccf3050ca2dbf9bf763ea2c352e
furkanevin <furkanevin00@gmail.com>
2024-08-22 11:33:58 +0300

    update

--- 12_typescript_projects/1_car_rental/src/utils/fetchCars.ts
+++ 12_typescript_projects/1_car_rental/src/utils/fetchCars.ts
@@ -9,14 +9,29 @@ const options = {
 };
 
 // Fonksiyon asenkron olduğundan ve bir return değerine sahip olduğundan dolayo return tipini belirlerken sadece return edilen tipi "CarType[]" ifade etmek yerine bu fonksiyonun hata da döndürebilecğeinden ve asenkdron olduğunda dolayı bu tipi react'ın içerisnde bulnan Promise tipine generic olarak göndererek return tipini belirledik
-const fetchCars = async (): Promise<CarType[]> => {
-  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=m4";
 
-  const res = await fetch(url, options);
+type Parameters = {
+  limit: number;
+  make?: string;
+  model?: string;
+};
 
-  const data = await res.json();
+const fetchCars = async ({
+  limit,
+  make = "bmw",
+  model = "m4",
+}: Parameters): Promise<CarType[]> => {
+  try {
+    const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${make}&model=${model}&limit=${limit}`;
 
-  return data;
+    const res = await fetch(url, options);
+
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    throw new Error("hata");
+  }
 };
 
 export default fetchCars;
