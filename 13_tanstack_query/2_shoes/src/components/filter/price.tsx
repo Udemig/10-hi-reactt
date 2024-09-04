4b25db2b36b229a834a98299ebea3485bbb8d32f
furkanevin <furkanevin00@gmail.com>
2024-09-05 00:37:33 +0300

    update

---
+++ 13_tanstack_query/2_shoes/src/components/filter/price.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { GenderProps } from "./gender";
+import { useSearchParams } from "react-router-dom";
+
+const Price = ({ selected, setSelected }: GenderProps) => {
+  const [params, setParams] = useSearchParams();
+
+  useEffect(() => {
+    if (Number(selected) > 0) {
+      params.set("price", selected);
+    } else {
+      params.delete("price");
+    }
+
+    setParams(params);
+  }, [selected]);
+
+  return (
+    <div>
+      <h2 className="mb-4 font-semibold">Fiyat</h2>
+
+      <input
+        value={selected}
+        onChange={(e) => setSelected(e.target.value)}
+        className="w-full"
+        type="range"
+        min={0}
+        max={1000}
+      />
+
+      <div className="flex justify-between font-open font-semibold">
+        <span>${selected}</span>
+        <span>$1000</span>
+      </div>
+    </div>
+  );
+};
+
+export default Price;
--- 13_tanstack_query/2_shoes/src/components/header/index.tsx
+++ 13_tanstack_query/2_shoes/src/components/header/index.tsx
@@ -1,25 +1,33 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header className="flex justify-between items-center bg-white-fa p-4 md:p-5 lg:p-6 xl:p-8 text-gray-dark">
-      <nav className="flex gap-10 font-semibold">
+    <header className="flex justify-between items-center bg-white-fa p-4 md:p-5 lg:p-6 xl:p-8 text-gray-dark rounded-[12px] md:rounded-[16px] lg:rounded-[20px] xl:rounded-[24px] mb-6 md:mb-10 lg:mb-15 xl:mb-20">
+      <nav className="flex gap-10 font-semibold max-lg:hidden">
         <a>New Drops 🔥</a>
         <a>Men</a>
         <a>Women</a>
       </nav>
 
-      <img className="w-[16%]" src="logo.svg" />
+      <button className="lg:hidden">
+        <img src="/hamburger.svg" />
+      </button>
 
-      <div className="flex items-center gap-10">
+      <Link to="/" className="w-[16%] max-w-[128px] max-h-[32px]">
+        <img className="size-full" src="logo.svg" />
+      </Link>
+
+      <div className="flex items-center gap-5 lg:gap-10">
         <img
           className="size-4 md:size-5 lg:size-6 xl:size-7"
           src="/user.svg"
         />
         <img
-          className="size-4 md:size-5 lg:size-6 xl:size-7"
+          className="size-4 md:size-5 lg:size-6 xl:size-7 max-lg:hidden"
           src="/search.svg"
         />
 
-        <span className="size-[25px] bg-yellow rounded-full grid place-items-center font-open font-semibold">
+        <span className="size-[25px] md:size-[30px] lg:size-[40px] md:text-lg  bg-yellow rounded-full grid place-items-center font-open font-semibold">
           0
         </span>
       </div>
